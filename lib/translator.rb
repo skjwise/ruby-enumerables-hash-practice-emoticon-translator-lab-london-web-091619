7cdfaed435ddff2a2399d2eef327dd22230d690e
Sarah Wise <skjwise@hotmail.com>
2019-08-31 14:27:04 +0000

    Automatically backed up by Learn

--- lib/translator.rb
+++ lib/translator.rb
@@ -21,6 +21,7 @@ def get_japanese_emoticon(file_path = './lib/emoticons.yml', english_emoticon)
   end
 end
 
-def get_english_meaning(file_path = '')
-  # code goes here
+def get_english_meaning(file_path = './lib/emoticons.yml', japanese_emoticon)
+  library = load_library
+  
 end
